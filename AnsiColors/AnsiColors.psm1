f705b1e7611fa792a16b1e02e077e3500f847412
Nabil Redmann <repo@bananaacid.de>
2026-04-09 22:06:58 +0200

    fix import path

--- AnsiColors/AnsiColors.psm1
+++ AnsiColors/AnsiColors.psm1
@@ -1,9 +1,9 @@
-. .\AnsiColors.ps1
+. $PSScriptRoot\AnsiColors.ps1
 
 # ConvertTo-AnsiColorString
 Set-Alias -Name AnsiString -Value ConvertTo-AnsiColorString
 
-. .\FromEscapedString.ps1
+. $PSScriptRoot\FromEscapedString.ps1
 
 Set-Alias -Name AnsiEscString -Value ConvertFrom-AnsiEscapedString
 
